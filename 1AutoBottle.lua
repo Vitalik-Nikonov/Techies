4670b9822b1fcf0231c7bd16a1949b1fa83f4c97
Vitalik-Nikonov <vitalik.nikonov@mail.ru>
2018-06-07 18:06:29 +0300

    Update 1AutoBottle.lua

--- 1AutoBottle.lua
+++ 1AutoBottle.lua
@@ -3,12 +3,18 @@ local AutoBottle = {}
 
 AutoBottle.optionEnable = Menu.AddOptionBool({ "Utility", "AutoUse" }, "Auto Bottle")
     function AutoBottle.OnUpdate()
-	if not Menu.IsEnabled(AutoBottle.optionEnable) then return end
+	    local MyHero = Heroes.GetLocal()
+    if not MyHero or not Entity.IsAlive(MyHero) or NPC.IsStunned(MyHero) or NPC.IsSilenced(MyHero) then
+        return
+    end
+	if not Menu.IsEnabled(AutoBottle.optionEnable) then 
+		return 
+	end
 	local myHero = Heroes.GetLocal()
 	local Use = NPC.GetItem(myHero, "item_bottle", true)
 	if Menu.IsEnabled(AutoBottle.optionEnable) then
-        if NPC.GetMaxMana(MyHero) - NPC.GetMana(MyHero) > 180 and AutoBottle and Ability.IsReady(AutoBottle) then
-            Ability.CastTarget(AutoBottle)
+        if NPC.GetMaxMana(MyHero) and AutoBottle and Ability.IsReady(AutoBottle) then
+            Ability.CastNoTarget(AutoBottle)
             return
         end
     end	
