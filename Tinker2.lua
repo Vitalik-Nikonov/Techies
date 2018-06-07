929e9ed7d24a3c94098b80470284a18397a39c2b
Vitalik-Nikonov <vitalik.nikonov@mail.ru>
2018-06-07 18:39:12 +0300

    Update Tinker2.lua

--- Tinker2.lua
+++ Tinker2.lua
@@ -9,3 +9,31 @@ Tinker.Items[1] =  "Off"
 -- Available items
 Tinker.Items[2] =  "Soul Ring"
 Tinker.Items[3] =  "Bottle"
+
+-- Tinker.ExtraSoul
+-- Tinker.ExtraBottle
+		if Menu.IsEnabled(Tinker.ExtraSoul) then
+			local sr = NPC.GetItem(Tinker.Hero, "item_soul_ring", true)
+			if	sr ~= nil 
+				and Ability.IsCastable(sr, Tinker.ManaPoint)
+				and Entity.GetHealth(Tinker.Hero) > Menu.GetValue(Tinker.ExtraSoulT)
+			then
+				Ability.CastNoTarget(sr)
+			end
+		end
+		
+		if Menu.IsEnabled(Tinker.ExtraBottle) then
+			local bott = NPC.GetItem(Tinker.Hero, "item_bottle", true)
+			if	bott ~= nil 
+				and Ability.IsCastable(bott, Tinker.ManaPoint)
+				and (Entity.GetHealth(Tinker.Hero) < Entity.GetMaxHealth(Tinker.Hero) or Tinker.ManaPoint < NPC.GetMaxMana(Tinker.Hero))
+				and not NPC.HasModifier(Tinker.Hero, "modifier_bottle_regeneration")
+			then
+				Ability.CastNoTarget(bott)
+			end
+		end
+	end
+	
+	
+	return true
+end
