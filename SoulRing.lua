56b368d352078f88573f81ea85f1a3ac36dd5836
Vitalik-Nikonov <vitalik.nikonov@mail.ru>
2018-06-08 13:32:17 +0300

    Update SoulRing.lua

--- SoulRing.lua
+++ SoulRing.lua
@@ -1,127 +1,85 @@
-local CrystalMaiden = {}
+local TinkerSB = {}
 
-CrystalMaiden.optionEnable = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden" }, "Enable", false)
-CrystalMaiden.optionKey = Menu.AddKeyOption({ "Hero Specific", "Crystal Maiden" }, "Combo Key", Enum.ButtonCode.KEY_1)
+CrystalMaiden.optionEnable = Menu.AddOptionBool({ "Hero Specific", "TinkerSB" }, "Enable", false)
+CrystalMaiden.optionKey = Menu.AddKeyOption({ "Hero Specific", "TinkerSB" }, "Combo Key", Enum.ButtonCode.KEY_1)
 
-CrystalMaiden.optionEscapeGlimmer = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden", "Auto escape" }, "Glimmer", false)
+-- CrystalMaiden.AddBKB           = Menu.AddOptionBool({"Hero Specific", "TinkerSB", "Combo skills"}, "Crystal Nova", false)
 
--- CrystalMaiden.AddBKB           = Menu.AddOptionBool({"Hero Specific", "Crystal Maiden", "Combo skills"}, "Crystal Nova", false)
+CrystalMaiden.AddBKB = Menu.AddOptionBool({ "Hero Specific", "TinkerSB", "Combo items" }, "Bottle", false)
+CrystalMaiden.AddShiva = Menu.AddOptionBool({ "Hero Specific", "TinkerSB", "Combo items" }, "SoulRing", false)
 
-CrystalMaiden.AddBKB = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden", "Combo items" }, "BKB", false)
-CrystalMaiden.AddGlimmer = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden", "Combo items" }, "Glimmer", false)
-CrystalMaiden.AddShiva = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden", "Combo items" }, "Shiva", false)
-
-CrystalMaiden.optionDebug = Menu.AddOptionBool({ "Hero Specific", "Crystal Maiden" }, "Debug", false)
+CrystalMaiden.optionDebug = Menu.AddOptionBool({ "Hero Specific", "TinkerSB" }, "Debug", false)
 
 local last_cast_time = 0
 
-function CrystalMaiden.OnUpdate()
-    if not Menu.IsEnabled(CrystalMaiden.optionEnable) then
+function TinkerSB.OnUpdate()
+    if not Menu.IsEnabled(TinkerSB.optionEnable) then
         return
     end
 
     local MyHero = Heroes.GetLocal()
-    if not MyHero or NPC.GetUnitName(MyHero) ~= "npc_dota_hero_crystal_maiden" then
+    if not MyHero or NPC.GetUnitName(MyHero) ~= "npc_dota_hero_tinker" then
         return
     end
     if not Entity.IsAlive(MyHero) or NPC.IsStunned(MyHero) or NPC.IsSilenced(MyHero) then
         return
     end
 
-    if Menu.IsEnabled(CrystalMaiden.optionEscapeGlimmer) then
-        if (os.clock() - last_cast_time) <= (10 + 3) then
-            local manaCount = NPC.GetMana(MyHero)
-            if not manaCount then
-                return
-            end
-
-            local glimmer = NPC.GetItem(MyHero, "item_glimmer_cape")
-            if not glimmer then
-                return
-            end
-
-            local glimmerManaCost = Ability.GetManaCost(glimmer)
-            if not glimmerManaCost then
-                return
-            end
-
-            if Ability.IsCastable(glimmer, manaCount) and Ability.IsReady(glimmer) then
-                Ability.CastTarget(glimmer, MyHero, true)
-                return
-            end
-        end
-    end
-
-    if Menu.IsKeyDownOnce(CrystalMaiden.optionKey) then
-        CrystalMaiden.Combo(MyHero)
+    if Menu.IsKeyDownOnce(TinkerSB.optionKey) then
+        TinkerSB.Combo(MyHero)
     end
 end
 
 function CrystalMaiden.Combo(MyHero)
-    local freezingField = NPC.GetAbility(MyHero, "crystal_maiden_freezing_field")
+    local rearm = NPC.GetAbility(MyHero, "tinker_rearm")
     -- local crystalNova   = NPC.GetAbility(MyHero, "crystal_maiden_crystal_nova")
-    local bkb = NPC.GetItem(MyHero, "item_black_king_bar")
-    local glimmer = NPC.GetItem(MyHero, "item_glimmer_cape")
-    local shiva = NPC.GetItem(MyHero, "item_shivas_guard")
+    local bottle = NPC.GetItem(MyHero, "item_bottle")
+    local soulring = NPC.GetItem(MyHero, "item_soul_ring")
 
-    if not freezingField then
+    if not rearm then
         return
     end
 
-    CrystalMaiden.manaCount = NPC.GetMana(MyHero)
-    CrystalMaiden.realManaCount = CrystalMaiden.manaCount
+    TinkerSB.manaCount = NPC.GetMana(MyHero)
+    TinkerSB.realManaCount = TinkerSB.manaCount
     if not CrystalMaiden.manaCount then
         return
     end
 
-    freezingFieldManaCost = Ability.GetManaCost(freezingField)
-    if not freezingFieldManaCost then
+    rearmBManaCost = Ability.GetManaCost(rearm)
+    if not rearmManaCost then
         return
     end
 
-    CrystalMaiden.manaCount = CrystalMaiden.manaCount - freezingFieldManaCost
-    CrystalMaiden.ManaNeed = CrystalMaiden.GetManaNeed(MyHero, bkb, glimmer, shiva)
+    TinkerSB.manaCount = TinkerSB.manaCount - rearmManaCost
+    TinkerSB.ManaNeed = TinkerSB.GetManaNeed(MyHero, bottle,  soulring)
 
-    if CrystalMaiden.manaCount >= CrystalMaiden.ManaNeed then
-        if bkb and Menu.IsEnabled(CrystalMaiden.AddBKB) and Ability.IsCastable(bkb, CrystalMaiden.manaCount) and Ability.IsReady(bkb) then
-            if Menu.IsEnabled(CrystalMaiden.optionDebug) then Log.Write("Use BKB") end
-            Ability.CastNoTarget(bkb, true)
+    if TinkerSB.manaCount >= TinkerSB.ManaNeed then
+        if bkb and Menu.IsEnabled(TinkerSB.Addbottle) and Ability.IsCastable(bottle, TinkerSB.manaCount) and Ability.IsReady(bottle) then
+            if Menu.IsEnabled(TinkerSB.optionDebug) then Log.Write("Use Bottle") end
+            Ability.CastNoTarget(bottle, true)
         end
 
-        if glimmer and Menu.IsEnabled(CrystalMaiden.AddGlimmer) and Ability.IsCastable(glimmer, CrystalMaiden.manaCount) and Ability.IsReady(glimmer) then
-            if Menu.IsEnabled(CrystalMaiden.optionDebug) then Log.Write("Use Glimmer cape") end
-            Ability.CastTarget(glimmer, MyHero, true)
-        end
 
-        if shiva and Menu.IsEnabled(CrystalMaiden.AddShiva) and Ability.IsCastable(shiva, CrystalMaiden.manaCount) and Ability.IsReady(shiva) then
-            if Menu.IsEnabled(CrystalMaiden.optionDebug) then Log.Write("Use Shiva's guard") end
-            Ability.CastNoTarget(shiva, true)
+        if shiva and Menu.IsEnabled(TinkerSB.Addsoulring) and Ability.IsCastable(shiva, TinkerSB.manaCount) and Ability.IsReady(soulring) then
+            if Menu.IsEnabled(TinkerSB.optionDebug) then Log.Write("Use Soul Ring") end
+            Ability.CastNoTarget(soulring, true)
         end
     end
 
-    if Menu.IsEnabled(CrystalMaiden.optionDebug) then Log.Write("Cast Ult") end
-    if freezingField and Ability.IsCastable(freezingField, CrystalMaiden.realManaCount) and Ability.IsReady(freezingField) then
-        Ability.CastNoTarget(freezingField, true)
-        last_cast_time = os.clock()
-    end
-end
-
-function CrystalMaiden.GetManaNeed(MyHero, bkb, glimmer, shiva)
+function TinkerSB.GetManaNeed(MyHero, bottle, soulring)
     local mana = 0
 
-    if bkb and Menu.IsEnabled(CrystalMaiden.AddBKB) then
+    if bottle and Menu.IsEnabled(TinkerSB.Addbottle) then
         mana = mana + Ability.GetManaCost(bkb)
     end
 
-    if glimmer and Menu.IsEnabled(CrystalMaiden.AddGlimmer) then
-        mana = mana + Ability.GetManaCost(glimmer)
-    end
 
-    if shiva and Menu.IsEnabled(CrystalMaiden.AddShiva) then
-        mana = mana + Ability.GetManaCost(shiva)
+    if soulring and Menu.IsEnabled(TinkerSB.Addsoulring) then
+        mana = mana + Ability.GetManaCost(soulring)
     end
 
     return mana
 end
 
-return CrystalMaiden
+return TinkerSB
